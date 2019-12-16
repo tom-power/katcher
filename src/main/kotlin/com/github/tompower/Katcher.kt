a01af6eeb4a30d47f76d3ae894d0abb679309959
Tom Power <tpower21@gmail.com>
2019-12-16 10:17:10 +0000

    Add some comments

--- src/main/kotlin/com/github/tompower/Katcher.kt
+++ src/main/kotlin/com/github/tompower/Katcher.kt
@@ -1,9 +1,16 @@
 package com.github.tompower
 
+/**
+ * Class for using regex to do something like pattern matching with case classes in [Scala](https://docs.scala-lang.org/tour/pattern-matching.html).
+ */
 @Suppress("UNCHECKED_CAST")
 class Katcher {
     private var matches: List<String> = emptyList()
 
+    /**
+     * Overloaded in keyword for regex matching
+     * @return true if matched
+     */
     operator fun String.contains(text: CharSequence): Boolean =
         this.toRegex().matches(text)
             .also { matches ->
@@ -12,10 +19,22 @@ class Katcher {
                 }
             }
 
+    /**
+     * Matched capture groups
+     * @return List<String>
+     */
     fun matches() = matches
 
+    /**
+     * One typed matched capture group
+     * @return Matches1<A>
+     */
     inline fun <reified A> matches1(): Matches1<A> = Matches1(matches().component1().toType())
 
+    /**
+     * Two typed matched capture groups
+     * @return Matches2<A, B>
+     */
     inline fun <reified A, reified B> matches2(): Matches2<A, B> =
         matches().let {
             Matches2(
@@ -24,6 +43,10 @@ class Katcher {
             )
         }
 
+    /**
+     * Three typed matched capture groups
+     * @return Matches3<A, B, C>
+     */
     inline fun <reified A, reified B, reified C> matches3(): Matches3<A, B, C> =
         matches().let {
             Matches3(
@@ -33,6 +56,10 @@ class Katcher {
             )
         }
 
+    /**
+     * Four typed matched capture groups
+     * @return Matches4<A, B, C, D>
+     */
     inline fun <reified A, reified B, reified C, reified D> matches4(): Matches4<A, B, C, D> =
         matches().let {
             Matches4(
@@ -59,6 +86,12 @@ data class Matches2<A, B>(val first: A, val second: B)
 data class Matches3<A, B, C>(val first: A, val second: B, val third: C)
 data class Matches4<A, B, C, D>(val first: A, val second: B, val third: C, val forth: D)
 
+/**
+ * Function literal with Katcher as receiver for matching and processing text input
+ * Passed lambda has available
+ *  - an overloaded in keyword for regex matching
+ *  - matches and matchesX methods to help with destructuring capture groups
+ */
 infix fun <T> CharSequence.match(block: Katcher.(text: CharSequence) -> T): T = Katcher().block(this)
 
 
